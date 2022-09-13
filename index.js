49ab338b76ef019bd8c72d7e0b3c728dc696e7bf
ErickMoseDev <erickmongaremose@gmail.com>
2022-09-13 16:54:13 +0300

    Finished Lab

---
+++ index.js
@@ -0,0 +1,18 @@
+
+function receivesAFunction(receiveMe) {
+    receiveMe();
+
+}
+
+
+function returnsANamedFunction() {
+    let myOrder = function () { }
+
+    return myOrder;
+}
+
+function returnsAnAnonymousFunction() {
+
+    return function () { }
+
+}
